151a5f80316998e793f7364c8a8f7be911657db9
Erik Shandrowski <erik843@gmail.com>
2014-07-23 16:04:55 -0600

    Done except for Jasmine testing

--- main.js
+++ main.js
@@ -1,53 +1,18 @@
 var chunk = function(arr, num) {
-
-	var spliceAmount = 0;
-	var startSplice = 0;
 	
-	var arrChunk = [];
 	var answerArr = [];
-	// Following works for arr that can be evenly divided by num
-	console.log(arr.length % num);
-	if (arr.length % num === 0) {
-		spliceAmount = arr.length/num;
-		for(var i=0; i <= num; i++) {
-			if (arr.length >= spliceAmount) {
-				arrChunk = arr.splice(startSplice, spliceAmount);
-				answerArr.push(arrChunk);
-				arrChunk = [];
-			}
-		}		
-		console.log(answerArr[0]);
-		return answerArr;
-	}
-	else {
-		var amountInChunks = arr.length/num;	
-		var roundedAmountInChunks = Math.round(amountInChunks);
+	var sizeChunks = Math.floor(arr.length/num);	
+	var remainder = arr.length % num;
 
-		if (Math.max(amountInChunks, roundedAmountInChunks) === amountInChunks) {
-			spliceAmount = roundedAmountInChunks + 1;
+	for(var i=0; i < num; i++) {		
+		var start = sizeChunks * i + remainder;
+		var end = start + sizeChunks;
+
+		if (remainder > 0 && i < remainder) {
+			start += i - remainder;
+			end = start + sizeChunks + 1;
 		}
-		else {
-			spliceAmount = roundedAmountInChunks;
+		answerArr[i] = arr.slice(start,end)
 		}
-
-		var chunksLeft = num;
-
-	// var numberOfFullChunks = Math.round(num/spliceAmount);
-
-	for(var i=0; i <= num; i++) {
-		if (arr.length >= spliceAmount) {
-			arrChunk = arr.splice(startSplice, spliceAmount);
-			answerArr.push(arrChunk);
-			arrChunk = [];
-		}
-	}
-
 	return answerArr;
 }
-
-var input = [1,2,3,4,5,6,7,8,9,10];
-var input2 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-
-$(document).on('ready', function() {
-  
-});
