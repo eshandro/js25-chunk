1c7a9c9a47cc4107fad0fad82ebdc8fbffc27fb7
eshandro <erik843@gmail.com>
2014-07-23 21:51:54 -0600

    refactor chunk solution

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ var chunk = function(arr, num) {
 	var sizeChunks = Math.floor(arr.length/num);	
 	var remainder = arr.length % num;
 
-	for(var i=0; i < num; i++) {		
+	for(var i=0; i < num; i++) {	
 		var start = sizeChunks * i + remainder;
 		var end = start + sizeChunks;
-
+		// When there is a remainder, must acct for it 
+		// in each chunk until on the chunk that is greater 
+		// than the amount of the remainder
 		if (remainder > 0 && i < remainder) {
 			start += i - remainder;
 			end = start + sizeChunks + 1;
