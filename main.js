e99b258c1f9f197c1cde1c452b8530b5789bf948
Erik Shandrowski <erik843@gmail.com>
2014-07-24 16:29:05 -0600

    Cleaned up chunk

--- main.js
+++ main.js
@@ -7,9 +7,8 @@ var chunk = function(arr, num) {
 	for(var i=0; i < num; i++) {	
 		var start = sizeChunks * i + remainder;
 		var end = start + sizeChunks;
-		// When there is a remainder, must acct for it 
-		// in each chunk until on the chunk that is greater 
-		// than the amount of the remainder
+		// Add in remainder one at a time into the first chunks 
+		// till remainder gone.
 		if (remainder > 0 && i < remainder) {
 			start += i - remainder;
 			end = start + sizeChunks + 1;
--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,6 @@
-describe('module', function() {
+describe('chunk', function() {
   it('should do something', function() {
+    
     expect(true).toEqual(false);
   });
 });
